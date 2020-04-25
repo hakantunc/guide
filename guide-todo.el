1e349ff4996bc9439a48b510185418977b68327c
Hakan Tunc <hakan.tunc@live.com>
2020-04-25 13:12:10 -0700

    Move org subtree down by a property value

--- guide-todo.el
+++ guide-todo.el
@@ -7,5 +7,14 @@
 
 ;;; Code:
 
+(defun guide-v2-move-down ()
+  (interactive)
+  (cl-loop repeat (guide-v2-get-fixed-schedule) do
+           (org-metadown)))
+
+(defun guide-v2-get-fixed-schedule ()
+  (string-to-number
+   (org-entry-get nil "GuideFixedSchedule")))
+
 (provide 'guide-todo)
 ;;; guide-todo.el ends here
