53e6ffba0a5fa6b4c18753c4b3d065809cadf810
Hakan Tunc <hakan.tunc@live.com>
2020-04-25 13:03:23 -0700

    Init

---
+++ guide-todo.el
@@ -0,0 +1,11 @@
+;;; guide-todo.el --- Guide Todo Minor Mode          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Hakan Tunc
+
+;; Author: Hakan Tunc
+;; Keywords: calendar
+
+;;; Code:
+
+(provide 'guide-todo)
+;;; guide-todo.el ends here
