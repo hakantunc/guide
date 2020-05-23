81d5ec26b08b0f15eb1cfcbc35af74f1de7d6867
Hakan Tunc <hakan.tunc@live.com>
2020-05-23 15:24:14 -0700

    Rename

--- guide-todo.el
+++ guide-todo.el
@@ -13,28 +13,28 @@
 (defconst guide-history "GuideHistory")
 (defconst guide-schedule-fixed "GuideScheduleFixed")
 
-(defun guide-v2-get-history ()
+(defun guide-get-history ()
   (org-entry-get-multivalued-property nil guide-history))
 
-(defun guide-v2-prepend-date ()
+(defun guide-prepend-date ()
   (let ((field-name guide-history))
     (apply
      #'org-entry-put-multivalued-property
      nil
      field-name
      (append (list (format-time-string "%F"))
-             (guide-v2-get-history)))))
+             (guide-get-history)))))
 
-(defun guide-v2-get-fixed-schedule ()
+(defun guide-get-fixed-schedule ()
   (let ((gsf (org-entry-get nil guide-schedule-fixed)))
     (if gsf
         (string-to-number gsf)
       nil)))
 
-(defun guide-v2-move-down ()
+(defun guide-move-down ()
   (interactive)
   (save-excursion
-    (let ((amount (guide-v2-get-fixed-schedule)))
+    (let ((amount (guide-get-fixed-schedule)))
       (condition-case nil
           (if amount
               (cl-loop repeat amount do
@@ -43,17 +43,17 @@
               (org-metadown)))
         (user-error nil)))))
 
-(defun guide-v2-move-down-with-date ()
+(defun guide-move-down-with-date ()
   (interactive)
-  (guide-v2-prepend-date)
-  (guide-v2-move-down))
+  (guide-prepend-date)
+  (guide-move-down))
 
-(defun guide-v2-get-top-level-history ()
+(defun guide-get-top-level-history ()
   (let ((entries (-non-nil
                   (org-map-entries
                    (lambda ()
                      (if (equal (org-current-level) 1)
-                         (guide-v2-get-history)
+                         (guide-get-history)
                        nil)) nil nil))))
     (-flatten entries)))
 
